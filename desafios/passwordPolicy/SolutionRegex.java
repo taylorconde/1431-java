14ee02cf80c96bf4734da9f4511648767045eee3
Marcel Tanuri <marcel.tanuri@gmail.com>
2025-08-15 22:11:31 -0300

    fix nome class

--- desafios/passwordPolicy/SolutionRegex.java
+++ desafios/passwordPolicy/SolutionRegex.java
@@ -1,4 +1,4 @@
-class Solution{
+class SolutionRegex{
     public static void main(String args[]){
         String senha = args.length > 0 ? args[0] : "";
 
@@ -6,7 +6,7 @@ class Solution{
         boolean deveConterPeloMenosUmNumero = senha.matches(".*\\d.*");
         boolean deveConterPeloMenosUmaLetraMaiuscula = senha.matches(".*[A-Z].*");
         boolean deveConterPeloMenosUmaLetraMinuscula = senha.matches(".*[a-z].*");
-        boolean deveConterPeloMenosUmCaractereEspecial = senha.matches(".*[^a-zA-Z0-9].*");
+        boolean deveConterPeloMenosUmCaractereEspecial = senha.matches(".*[^a-zA-Z0-9\u00C0-\u00FF].*");
 
         boolean senhaValida = deveTerNoMinimoOitoCaracteres && deveConterPeloMenosUmaLetraMaiuscula && deveConterPeloMenosUmNumero && deveConterPeloMenosUmaLetraMinuscula && deveConterPeloMenosUmCaractereEspecial;
 
@@ -15,7 +15,5 @@ class Solution{
         }else{
             System.out.println("Senha inválida");
         }
-    
-
     } 
 }
